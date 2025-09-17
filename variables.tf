27fc72c89906cac1ef3a4fda15d3f11c0eb485dd
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-09-17 14:56:18 +0100

    max_concurrency as stirng

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "output_parquet_file_size" {
 }
 
 variable "max_concurrency" {
-  type        = number
+  type        = string
   description = "Maximum number of database-export lambda run in parallel."
-  default     = 5
+  default     = "5"
 }
