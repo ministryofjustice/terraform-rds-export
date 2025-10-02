b523358736651bb4d78ec918aae5066a9b055d1f
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-10-02 11:52:24 +0100

    test update

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -1,6 +1,19 @@
 
 module "module_test" {
+  providers = {
+    aws = aws
+  }
+  
   source           = "../../"
-  application_name = local.application_name
+
+  name                  = "test"
+  database_refresh_mode    = "test"
+  db_name = "test"
+  vpc_id                = "test"
+  database_subnet_ids   = ["test"]
+  kms_key_arn           = "test"
+  master_user_secret_id = "test"
+  environment              = "test"
+
   tags             = local.tags
 }
