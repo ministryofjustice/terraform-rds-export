1984e9d861d20f0f6cd43a617d21af7eb78c0a78
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-11-10 15:48:49 +0000

    updated outputs file

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "backup_uploads_s3_bucket_arn" {
     description = "ARN of the backup uploads bucket"
     value = module.s3-bucket-backup-uploads.bucket.arn
 }
+
+output "backup_uploads_s3_bucket_name" {
+    description = "Name of the backup uploads bucket"
+    value = module.s3-bucket-backup-uploads.bucket.id
+}
