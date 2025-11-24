123c0f47da634141db96678f4ad488ab3de9cec0
zerj9 <96551236+zerj9@users.noreply.github.com>
2025-11-24 14:27:12 +0000

    export lambda role arns as tf outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "sns_topic_arn" {
   description = "ARN of the SNS topic to subscribe to"
   value       = aws_sns_topic.sfn_events.arn
 }
+
+output "database_export_scanner_role_arn" {
+  description = "ARN of the IAM role for the database export scanner. This role is used to create glue tables"
+  value       = module.database_export_scanner.lambda_role_arn
+}
+
+output "database_export_processor_role_arn" {
+  description = "ARN of the IAM role for the database export processor. This role is used to export data to S3"
+  value       = module.database_export_processor.lambda_role_arn
+}
