b0699bf74abc728f7b87956504b5a19d56eb0a93
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-12-10 17:55:37 +0000

    iam updates for optional resources

--- iam.tf
+++ iam.tf
@@ -87,22 +87,26 @@ resource "aws_iam_policy" "allow_start_execution" {
           "states:DescribeStateMachine",
           "states:ListExecutions"
         ],
-        Resource = [
-          "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-export",
-          "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-export-views",
-          "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-delete"
-        ]
+        Resource = concat(
+          [
+            "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-export",
+            "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-delete"
+          ],
+          var.get_views ? ["arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-export-views"] : []
+        )
       },
       {
         Effect = "Allow",
         Action = [
           "states:DescribeExecution",
         ],
-        Resource = [
-          "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:execution:${var.name}-${var.environment}-database-export",
-          "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-export-views",
-          "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:execution:${var.name}-${var.environment}-database-delete"
-        ]
+        Resource = concat(
+          [
+            "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-export",
+            "arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-delete"
+          ],
+          var.get_views ? ["arn:aws:states:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.name}-${var.environment}-database-export-views"] : []
+        )
       }
     ]
   })
