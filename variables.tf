760ec96d07514a830d6d6cc1c442dc10c2c32105
Ant Fitzroy <101649764+AntFMoJ@users.noreply.github.com>
2026-05-21 14:05:26 +0100

    🛠️  Add Github actions to repo (#189)
    * add GH actions to repo
    
    * Refresh Terraform lockfile checksums
    
    * update .terraform.lock.hcl for linux_amd64
    
    * update typo handling
    
    * fix typo

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  description = "The name of the project. Combined with the environment (<name>-<evironment>) to create the RDS DB instance identifier."
+  description = "The name of the project. Combined with the environment (<name>-<environment>) to create the RDS DB instance identifier."
   type        = string
 }
 
